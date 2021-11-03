6072c44e9f7ee7c181bb47abb936cd20c6202cc1
Alexander Kharmansky <davilex_x@vk.com>
2021-11-03 19:17:30 +0300

    feat: add trim() method to 01-read-file task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,5 +2,5 @@ const fs = require('fs');
 const filePath = fs.createReadStream('01-read-file/text.txt');
 
 filePath.on('data', (data) => {
-    console.log(data.toString())
+    console.log(data.toString().trim())
 })
