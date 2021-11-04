0b902941f47006a34c0950644e2a2396673cf90d
Alexander Kharmansky <davilex_x@vk.com>
2021-11-04 15:23:02 +0300

    feat: complete 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,47 @@
+const fs = require('fs');
+const path = require('path');
+const folder = '04-copy-directory/files/';
+const folderCopy = '04-copy-directory/files-copy/';
 
+fs.readdir(folder, {withFileTypes: true}, (err, files) => {
+    if (err) {
+        throw err
+    }
+
+    fs.mkdir(path.join(__dirname, 'files-copy'), (err) => {
+        if (err) {
+            fs.rmdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+                if (err) {
+                    throw err
+                } else {
+                    fs.mkdir(path.join(__dirname, 'files-copy'), (err) => {
+                        if (err) {
+                            throw err
+                        }
+                        files.forEach(file => {
+                            let filePath = `${folder}${file.name}`;
+                            let filePathCopy = `${folderCopy}${file.name}`;
+                    
+                            fs.copyFile(filePath, filePathCopy, (err) => {
+                                if (err) {
+                                    throw err
+                                }
+                            })
+                        });
+                    })
+                }
+            });
+        }
+    })
+    
+    files.forEach(file => {
+        let filePath = `${folder}${file.name}`;
+        let filePathCopy = `${folderCopy}${file.name}`;
+
+        fs.copyFile(filePath, filePathCopy, (err) => {
+            if (err) {
+                throw err
+            }
+        })
+    });
+});
